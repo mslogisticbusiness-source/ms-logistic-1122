b84bd5d085be148a5cee9e23d1c7641816051366
mslogisticbusiness-source <mslogisticbusiness@gmail.com>
2025-12-14 16:54:44 +0530

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,12 +1,21 @@
-const API_URL = "https://ms-logistic-backend.onrender.com";
+const API_URL = "https://ms-logistic-backend-1.onrender.com";
 
 async function apiRequest(endpoint, method = "GET", data = null, token = null) {
   const options = {
-    method,
-    headers: { "Content-Type": "application/json" }
+    method: method,
+    headers: {
+      "Content-Type": "application/json"
+    }
   };
-  if (token) options.headers["Authorization"] = "Bearer " + token;
-  if (data) options.body = JSON.stringify(data);
-  const res = await fetch(API_URL + endpoint, options);
-  return res.json();
+
+  if (token) {
+    options.headers["Authorization"] = "Bearer " + token;
+  }
+
+  if (data) {
+    options.body = JSON.stringify(data);
+  }
+
+  const response = await fetch(API_URL + endpoint, options);
+  return response.json();
 }
